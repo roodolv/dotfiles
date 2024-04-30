cf64c8f2c9450c3539ef4e56368d5d39aceac0e7
roodolv <113752412+roodolv@users.noreply.github.com>
2024-04-30 22:38:27 +0900

    Added fzf-lua and tweaked

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,35 +16,26 @@ return {
       require("config/startify")
     end
   },
-  -- view
+  -- view (pane/tab/window)
   {
     "simeji/winresizer",
-    event = "VimEnter",
+    cmd = "WinResizerStartResize",
+    init = function()
+      vim.api.nvim_set_keymap('n', '<Leader>R', ':<C-u>WinResizerStartResize<CR>', { noremap = true, silent = true })
+    end,
     config = function()
       require("config/winresizer")
     end,
   },
   {
-    "Yggdroot/indentLine",
-    event = "BufRead",
-    config = function()
-      require("config/indentline")
-    end,
-  },
-  {
     "preservim/tagbar",
-    event = "BufRead",
+    cmd = "TagbarToggle",
+    init = function()
+      vim.api.nvim_set_keymap('n', '<Leader>gt', ':<C-u>TagbarToggle<CR>', { noremap = true, silent = true })
+    end,
     config = function()
       require("config/tagbar")
     end,
-  },
-  -- editing
-  {
-    "easymotion/vim-easymotion",
-    event = "BufRead",
-    config = function()
-      require("config/easymotion")
-    end
   },
   {
     "mbbill/undotree",
@@ -55,6 +46,70 @@ return {
     config = function()
       require("config/undotree")
     end,
+  },
+  -- search/filer
+  {
+    "ibhagwan/fzf-lua",
+    event = "VimEnter",
+    dependencies = {
+      "junegunn/fzf",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("config/fzf-lua")
+    end
+  },
+  -- {
+  --   "nvim-tree/nvim-tree.lua",
+  --   event = "VimEnter",
+  --   dependencies = {
+  --     "junegunn/fzf",
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --   config = function()
+  --     require("config/nvim-tree")
+  --   end
+  -- },
+
+  -- syntax/indentation
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "VimEnter",
+    config = function()
+      require("config/nvim-treesitter")
+    end,
+  },
+  {
+    "Yggdroot/indentLine",
+    event = "BufRead",
+    config = function()
+      require("config/indentline")
+    end,
+  },
+
+  -- LSP
+
+  -- AGI
+  -- repo = 'Exafunction/codeium.vim'
+  -- on_event = 'BufRead'
+  -- # hook_add = 'source ~/.config/nvim/plugins/codeium-vim.rc.vim'
+
+  -- Git
+  -- repo = 'rhysd/git-messenger.vim'
+  -- on_cmd = 'GitMessenger'
+  -- on_map = '<Plug>(git-messenger'
+  -- hook_add = '''
+  --   nnoremap <silent> <Leader>gm :<C-u>GitMessenger<CR>
+  -- '''
+  -- hook_source = 'source ~/.config/nvim/plugins/git-messenger.rc.vim'
+
+  -- editing
+  {
+    "easymotion/vim-easymotion",
+    event = "BufRead",
+    config = function()
+      require("config/easymotion")
+    end
   },
   {
     "machakann/vim-sandwich",
