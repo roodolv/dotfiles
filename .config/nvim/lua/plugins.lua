8e3919dea10fbfc9c4c076204362fb7622f0b6cc
roodolv <113752412+roodolv@users.noreply.github.com>
2024-05-10 02:22:49 +0900

    Added todo-comments.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -89,6 +89,7 @@ return {
   },
   {
     "ThePrimeagen/harpoon",
+    event = { "BufReadPre", "BufNewFile" },
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function ()
@@ -354,6 +355,48 @@ return {
     "bronson/vim-trailing-whitespace",
     keys = {
       { "<Leader>T", ":<C-u>FixWhitespace<CR>", mode= "n", silent = true },
+    },
+  },
+  -----------------------------------------------------------------
+  -- etc
+  -----------------------------------------------------------------
+  {
+    "folke/todo-comments.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      keywords = {
+        FIX = {
+          icon = "", -- icon used for the sign, and in search results
+          color = "error", -- can be a hex color, or a named color (see below)
+          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+          -- signs = false, -- configure signs for some keywords individually
+        },
+        TODO = { icon = "", color = "info" },
+        HACK = { icon = "", color = "warning" },
+        WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = "", color = "hint", alt = { "INFO", "MEMO" } },
+        TEST = { icon = "", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        REVW = { icon = "", color = "review", alt = { "REVIEW", "NOTICE" } },
+      },
+      merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+      highlight = {
+        before = "", -- "fg" or "bg" or empty
+        keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+        after = "fg", -- "fg" or "bg" or empty
+        pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+        exclude = {}, -- list of file types to exclude highlighting
+      },
+      colors = {
+        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+        info = { "DiagnosticInfo", "#2563EB" },
+        hint = { "DiagnosticHint", "#10B981" },
+        default = { "Identifier", "#7C3AED" },
+        test = { "Identifier", "#FF00FF" },
+        review = { "Identifier", "#777788" }
+      },
     },
   },
 }
