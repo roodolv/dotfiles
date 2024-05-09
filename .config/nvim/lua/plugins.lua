fdc79a132ed466ffc613c41c7b4fb2f681ab3c08
roodolv <113752412+roodolv@users.noreply.github.com>
2024-05-10 01:37:29 +0900

    Added harpoon2 & flash.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -28,7 +28,7 @@ return {
     keys = {
       { "<Leader>R", ":<C-u>WinResizerStartResize<CR>", mode = "n", silent = true },
     },
-    config = function()
+    init = function()
       require("config/winresizer")
     end,
   },
@@ -65,7 +65,49 @@ return {
   --     require("config/fzf-lua")
   --   end
   -- },
-  -- TODO: Add telescope.nvim, flash.nvim here
+  -- TODO: Add telescope.nvim here
+  {
+    "easymotion/vim-easymotion",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("config/easymotion")
+    end
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+    end
+  },
   -----------------------------------------------------------------
   -- filer/browser
   -----------------------------------------------------------------
@@ -265,13 +307,6 @@ return {
   -----------------------------------------------------------------
   -- editing
   -----------------------------------------------------------------
-  {
-    "easymotion/vim-easymotion",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("config/easymotion")
-    end
-  },
   {
     "machakann/vim-sandwich",
     event = { "BufReadPre", "BufNewFile" },
