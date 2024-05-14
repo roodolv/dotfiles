0fd93d1d7a35db10bfbd22dfca7fb5f95433150a
roodolv <113752412+roodolv@users.noreply.github.com>
2024-05-15 01:10:18 +0900

    Moved telescope's keymaps & changed some trigger event

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,4 +1,3 @@
--- setup telescope
 local Telescope = require("telescope")
 local actions = require("telescope.actions")
 
@@ -30,6 +29,36 @@ Telescope.setup({
     },
   },
 })
+
+vim.keymap.set("n", "<Leader>ff", ":<C-u>Telescope find_files<CR>", { silent = true, desc = "List project files" })
+vim.keymap.set("n", "<Leader>fg", ":<C-u>Telescope live_grep<CR>", { silent = true, desc = "Live Grep" })
+vim.keymap.set("n", "<Leader>fG", ":<C-u>Telescope git_files<CR>", { silent = true, desc = "List git_files" })
+vim.keymap.set("n", "<Leader>fl", ":<C-u>Telescope current_buffer_fuzzy_find<CR>", { silent = true, desc = "Current buffer fuzzy find" })
+vim.keymap.set("n", "<Leader>fj", function()
+  local jumplist = vim.fn.getjumplist()
+  require("telescope.builtin").jumplist({
+    on_complete = {
+      function(self)
+        -- select current
+        local n = #jumplist[1]
+        if n ~= jumplist[2] then
+          self:move_selection(jumplist[2] - #jumplist[1] + 1)
+        end
+      end,
+    },
+  })
+  end, { silent = true, desc = "List jumplist" }
+)
+vim.keymap.set("n", "<Leader>fb", ":<C-u>Telescope buffers<CR>", { silent = true, desc = "List buffers" })
+vim.keymap.set("n", "<Leader>fr", ":<C-u>Telescope oldfiles<CR>", { silent = true, desc = "List recent files" })
+vim.keymap.set("n", "<Leader>fh", ":<C-u>Telescope help_tags<CR>", { silent = true, desc = "List help-tags" })
+vim.keymap.set("n", "<Leader>fc", ":<C-u>Telescope commands<CR>", { silent = true, desc = "List commands" })
+vim.keymap.set("n", "<Leader>f:", ":<C-u>Telescope command_history<CR>", { silent = true, desc = "List command history" })
+vim.keymap.set("n", "<Leader>fm", ":<C-u>Telescope keymaps<CR>", { silent = true, desc = "List keymaps" })
+vim.keymap.set("n", "<Leader>fp", function() require("telescope").extensions.frecency.frecency { workspace = "CWD", } end, { silent = true, desc = "List prioritized by frecency" })
+vim.keymap.set("n", "<Leader>rr", function() require("telescope").extensions.refactoring.refactors() end, { silent = true, desc = "List refactoring methods" })
+vim.keymap.set("x", "<Leader>rr", function() require("telescope").extensions.refactoring.refactors() end, { silent = true, desc = "List refactoring methods" })
+-- vim.keymap.set("n", "<Leader>fs", function() require("telescope").extensions.smart_open.smart_open() end, { silent = true, desc = "List smart-open" })
 
 -- setup sqlite3 path for smart-open.nvim
 -- local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -62,35 +62,10 @@ return {
       "ThePrimeagen/refactoring.nvim",
       -- "danielfalk/smart-open.nvim",
     },
-    cmd = "Telescope",
     keys = {
-      { "<Leader>ff", function() require("telescope.builtin").find_files() end, desc = "List project files", mode = "n", silent = true },
-      { "<Leader>fg", function() require("telescope.builtin").live_grep() end, desc = "Live Grep", mode = "n", silent = true },
-      { "<Leader>fG", function() require("telescope.builtin").git_files() end, desc = "List git_files", mode = "n", silent = true },
-      { "<Leader>fl", function() require("telescope.builtin").current_buffer_fuzzy_find() end, desc = "Current buffer fuzzy find", mode = "n", silent = true },
-      { "<Leader>fj", function()
-        local jumplist = vim.fn.getjumplist()
-        require("telescope.builtin").jumplist({
-          on_complete = {
-            function(self)
-              -- select current
-              local n = #jumplist[1]
-              if n ~= jumplist[2] then
-                self:move_selection(jumplist[2] - #jumplist[1] + 1)
-              end
-            end,
-          },
-        })
-      end, desc = "List jump-list", mode = "n", silent = true },
-      { "<Leader>fb", function() require("telescope.builtin").buffers() end, desc = "List buffers", mode = "n", silent = true },
-      { "<Leader>fr", function() require("telescope.builtin").oldfiles() end, desc = "List recent files", mode = "n", silent = true },
-      { "<Leader>fh", function() require("telescope.builtin").help_tags() end, desc = "List help-tags", mode = "n", silent = true },
-      { "<Leader>fc", function() require("telescope.builtin").commands() end, desc = "List commands", mode = "n", silent = true },
-      { "<Leader>f:", function() require("telescope.builtin").command_history() end, desc = "List command history", mode = "n", silent = true },
-      { "<Leader>fm", function() require("telescope.builtin").keymaps() end, desc = "List keymaps", mode = "n", silent = true },
-      { "<Leader>fp", function() require("telescope").extensions.frecency.frecency { workspace = "CWD", } end, desc = "List prioritized by frecency algorithm", mode = "n", silent = true },
-      { "<Leader>rr", function() require("telescope").extensions.refactoring.refactors() end, desc = "List refactoring methods" , mode = {"n", "x"}, silent = true }
-      -- { "<Leader>fs", function() require("telescope").extensions.smart_open.smart_open() end, desc = "List prioritized by frecency algorithm", mode = "n", silent = true },
+      "<Leader>ff", "<Leader>fg", "<Leader>fG", "<Leader>fl", "<Leader>fj",
+      "<Leader>fb", "<Leader>fr", "<Leader>fh", "<Leader>fc", "<Leader>f:",
+      "<Leader>fm", "<Leader>fp", "<Leader>rr",
     },
     config = function ()
       require("config/telescope")
@@ -114,11 +89,13 @@ return {
   -- },
   {
     "ThePrimeagen/harpoon",
-    event = { "BufReadPre", "BufNewFile" },
     branch = "harpoon2",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      "<Leader>ht", "<Leader>hx",
     },
     config = function ()
       require("config/harpoon")
